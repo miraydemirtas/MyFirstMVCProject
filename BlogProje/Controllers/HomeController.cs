929063aec7d01098f869c8fab1254a45e814760b
miray <mirayfb19077@gmail.com>
2017-09-28 18:25:23 +0300

    general changes

---
+++ BlogProje/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using BlogProje.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        BlogDB db = new BlogDB();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult KategoriPartial()
+        {
+            db.Kategoris.ToList();
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
